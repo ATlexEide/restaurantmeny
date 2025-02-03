833476f5bc295865a1c0e11dbf03fca51606de99
Alex <atlexeide@gmail.com>
2025-02-03 13:14:37 +0100

    Add css import

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,3 +1,4 @@
+import "../styles/Dish.css";
 function Dish({ title, price, ingredients, category }) {
   // Kortkomponent (Dish): Denne komponenten mottar data for én matrett som props og viser dem i et kortformat. Bruk feltene for tittel, pris, ingredienser og kategori i visningen.
   return (
