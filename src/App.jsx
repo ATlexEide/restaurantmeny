6466f1c0bf5177437eb164a9c79c48450b72a654
Alex <atlexeide@gmail.com>
2025-02-03 13:03:56 +0100

    Remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Menu from "./components/Menu";
 import MenuData from "./data/data";
 function App() {
   return <Menu menu={MenuData} />;
-  // Hovedkomponenten (App): Denne komponenten importerer menydata og sender dem videre som props til en underkomponent.
 }
 
 export default App;
