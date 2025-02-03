466a7751f4b41b5059605244ab45c404029de1c8
Alex <atlexeide@gmail.com>
2025-02-03 13:13:42 +0100

    Add return to dish

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,4 +1,14 @@
-function Dish() {
+function Dish({ title, price, ingredients, category }) {
   // Kortkomponent (Dish): Denne komponenten mottar data for én matrett som props og viser dem i et kortformat. Bruk feltene for tittel, pris, ingredienser og kategori i visningen.
+  return (
+    <>
+      <h2>
+        {title}
+        <span>{price}</span>
+      </h2>
+      <p>{ingredients}</p>
+      <p>{category}</p>
+    </>
+  );
 }
 export default Dish;
