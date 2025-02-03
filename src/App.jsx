e52825ed101758d43250e1b01b6ca8acc4839ff6
Alex <atlexeide@gmail.com>
2025-02-03 13:00:05 +0100

    Add imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Menu from "./components/Menu";
 import "./styles/App.css";
-
+import Menu from "./components/Menu";
+import MenuData from "./data/data";
 function App() {
+  return <Menu menu={Meny} />;
   // Hovedkomponenten (App): Denne komponenten importerer menydata og sender dem videre som props til en underkomponent.
 }
 
